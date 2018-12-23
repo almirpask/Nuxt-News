e50f56fb507fa56d2a301f8ef36e98ccf3c9ad05
almirpask <almir.santos@umanni.com.br>
2018-12-23 02:49:08 -0200

    Add vue-material

--- nuxt.config.js
+++ nuxt.config.js
@@ -14,7 +14,9 @@ module.exports = {
       { hid: 'description', name: 'description', content: pkg.description }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel: 'stylesheet',  href: '//fonts.googleapis.com/css?family=Roboto:400,500,700,400italic|Material+Icons' }
+
     ]
   },
 
@@ -27,12 +29,14 @@ module.exports = {
   ** Global CSS
   */
   css: [
+    { src: 'vue-material/dist/vue-material.min.css', lang: 'css' }
   ],
 
   /*
   ** Plugins to load before mounting the App
   */
   plugins: [
+    {src: '~/plugins/vue-material'}
   ],
 
   /*
@@ -57,7 +61,7 @@ module.exports = {
     ** You can extend webpack config here
     */
     extend(config, ctx) {
-      
+
     }
   }
 }
